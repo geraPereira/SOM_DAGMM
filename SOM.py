4934ae68e65f91c5ecb24db1b469522b4ce25cc4
Geraldo <gpsj2@cin.ufpe.br>
2025-12-10 16:42:22 -0300

    modificações no eval

--- SOM.py
+++ SOM.py
@@ -1,7 +1,7 @@
 import numpy as np
 from minisom import MiniSom
 
-def som_train(data, x=10, y=10, sigma=1, learning_rate= 0.05, iters= 10000):
+def som_train(data, x=10, y=10, sigma=1, learning_rate= 0.06, iters= 10000):
     input_len = data.shape[1]
     #print("SOM training started:")
     som = MiniSom(x= x, y= y, input_len=input_len, sigma=sigma, learning_rate=learning_rate)
---
+++ run_job_eval.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=som_kdd
+#SBATCH --partition=gpu
+#SBATCH -p short-complex
+#SBATCH --qos=complex
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --time=2-00:00:00
+#SBATCH --output=logs/som_ids_%j.out
+#SBATCH --error=logs/som_ids_%j.err
+
+
+# Carrega o módulo do Miniforge
+module load miniforge3
+
+# Habilita o comando conda dentro do job
+source "$(conda info --base)/etc/profile.d/conda.sh"
+
+
+# Ir para o diretório do projeto
+cd $HOME/SOM_DAGMM
+
+# Só pra conferência
+nvidia-smi
+echo "Iniciando treino em $(hostname)"
+echo "Começou em $(date)"
+
+# Rodar o train
+python eval.py \
+  --dataset kdd \
+  --embed label_encode \
+  --features numerical \
+  --threshold 20
+
+echo "Terminou em $(date)"
